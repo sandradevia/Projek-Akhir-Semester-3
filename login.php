b195eb4f9a2fc1fe07df5b07bb889d74d2f9f41c
sandradevia <sandradeviarge@gmail.com>
2023-11-30 08:07:08 +0700

    projek akhir semester 3

---
+++ login.php
@@ -0,0 +1,131 @@
+<?php
+	@ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
+
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = md5(?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword">
+
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        
+    <!-- Custom styles for this template -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="
+  			background-repeat: no-repeat;
+            color:#fff;">
+
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+	  <form action="op.php" method="post">
+            <div class="container">
+                <section class="login">
+                    <div class="login-left">
+                        <h2>KASIRKU</h2>
+                        <img src="dist/img/login.png" alt="Logo" class="centered-image">
+                    </div>
+                    <div class="login-right">
+                        <img src="dist/img/logo.jpg" alt="Circle Logo" class="centered-image">
+                        <div class="login-menu">
+                            <h3>WELCOME</h3>
+                        </div>
+
+                        <div class="form-floating" data-validate="Masukan username">
+                            <input class="form-control" type="text" id="floatingInput"  name="txtuser" placeholder="username">
+                            <label for="floatingInput">Username</label>
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="form-floating" data-validate="Masukan password">
+                            <input class="form-control" type="password" id="floatingPassword"  name="txtpass" placeholder="password">
+                            <label for="floatingPassword">Password</label>
+                            <span class="focus-input100"></span>
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="rememberMe">
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                Login
+                            </button>
+                        </div>
+
+                        <div class="text-nowrap" style="width: 8rem;">
+                            <strong>
+                                &nbsp;&nbsp;&nbsp;
+                            </strong>
+                        </div>
+
+                        <span class="login100-form-title p-b-37">
+                            <p class="login-box-msg"> Point of Sales<br />Copyright © 2023</p>
+                        </span>
+
+                    </div>
+                </section>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+	  <div id="login-page" style="padding-top:3pc;">
+	  	<div class="container">
+		      <form class="form-login" method="POST">
+		        <h2 class="form-login-heading">Login</h2>
+		        <div class="login-wrap">
+		            <input type="text" class="form-control" name="user" placeholder="User ID" autofocus>
+		            <br>
+		            <input type="password" class="form-control" name="pass" placeholder="Password">
+		            <br>
+		            <button class="btn btn-primary btn-block" name="proses" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
+		        </div>
+		      </form>	  	
+	  	
+	  	</div>
+	  </div>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
